1135aa269c70149e3f3409f48e15a52770b5a1b4
igorhscode <59455439+igorhscode@users.noreply.github.com>
2020-02-10 19:32:49 +0300

    Update role.js

--- role.js
+++ role.js
@@ -6,7 +6,7 @@ const client = new Client({ disableEveryone: true });
 if (CONFIG.botToken === '')
     throw new Error("The 'botToken' property is not set in the config.js file. Please do this!");
 
-client.login(CONFIG.botToken);
+client.login(process.env.BOT_TOKEN);
 
 // If there isn't a reaction for every role, alert the user
 if (CONFIG.roles.length !== CONFIG.reactions.length)
