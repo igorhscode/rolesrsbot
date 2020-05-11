64a164e028b85bff426fb977bc56743efd5b989d
igorhscode <59455439+igorhscode@users.noreply.github.com>
2020-05-11 20:32:29 +0300

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -29,7 +29,7 @@ module.exports = {
 	/**
 	 * For custom emojis, provide the name of the emoji
 	 */
-	reactions: ["ann", "bw", "sw", "duels", "hg", "mw", "bb", "prison", "arc", "kpvp", "bp", "cp", "prison", "arc", "gg", "dr"],
+	reactions: ["ann", "bw", "sw", "duels", "hg", "mw", "bb", "prison", "arc", "kpvp", "bp", "cp", "gg", "dr"],
 
 	/**
 	 * Set to "true" if you want all roles to be in a single embed
